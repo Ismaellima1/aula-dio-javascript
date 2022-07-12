2dec517f01c5a1620cdd00ec90295f4923b358da
ismael lima <ismaellimadeolivera2004@gmail.com>
2022-07-12 20:27:51 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+/*
+function validarIdade(idade){
+ if(idade >= 18){
+  document.write('você e maior de iadade');
+ }else{
+  document.write('você e de menor');
+ }
+}
+validarIdade(prompt('qual e sua idade'));
+*/
+
+
+/* new Date serve para mostrar a data
+let d = new Date();
+hora = d.getHours(); < pega a hora da data
+minut = d.getMinutes();
+seconds = d.getSeconds();
+*/
+
+
+/* Estrutura de repetição usando for(variavel;condição;aqui e o que acontece se a condição for true)
+for(count = 0;count <= 5;count++){
+ console.log(count);
+}
+*/
+
+/* Estrutura de repetição (while vai ficar em loop até a condição se tornar falsa.)
+let count = 0;
+while(count <= 10){
+ console.log(count);
+ count = count + 1;
+}
+*/
+
+/*
+let fruta = ["uva","maçã","manga"];
+fruta.toString();
+console.log(fruta.join([sparator = " > "]));
+*/
+
+/*
+let fruta = {nome: "maçã",cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+let lista = ["uva","pera","maçã"]
+lista.push('manga');                < // adciona elemento ao Array
+lista.pop();                        < // remove o ultimo elemento do Array
+console.log(lista);
+*/
+
+
